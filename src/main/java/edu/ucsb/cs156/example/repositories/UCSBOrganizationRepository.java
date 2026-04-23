6b8c34614271cc98519d30a349eee0e010a7447f
Pranav Gunhal <pgunhal@ucsb.edu>
2026-04-22 19:11:18 -0700

    style change for testing

--- src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
+++ src/main/java/edu/ucsb/cs156/example/repositories/UCSBOrganizationRepository.java
@@ -5,9 +5,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-/** 
- * @author: pgunhal
- * The UCSBOrganizationRepository is a repository for UCSBOrganization entities */
+/**
+ * @author: pgunhal The UCSBOrganizationRepository is a repository for UCSBOrganization entities
+ */
 @Repository
 @RepositoryRestResource(exported = false)
 public interface UCSBOrganizationRepository extends CrudRepository<UCSBOrganization, String> {}
