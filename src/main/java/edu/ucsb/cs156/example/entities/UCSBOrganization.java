36be715020f1f2621d679e39115bbbd2c296f65d
Pranav Gunhal <pgunhal@ucsb.edu>
2026-04-23 17:20:35 -0700

    fix bugs with the name of databse table

--- src/main/java/edu/ucsb/cs156/example/entities/UCSBOrganization.java
+++ src/main/java/edu/ucsb/cs156/example/entities/UCSBOrganization.java
@@ -1,28 +1,23 @@
 package edu.ucsb.cs156.example.entities;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * @author: pgunhal
- * This is a JPA entity that represents a UCSBOrganization, a student organization at UCSB.  It is used to store the results from the UCSB API for student organizations.
+ * @author: pgunha This is a JPA entity that represents a UCSBOrganization
+ *     <p>A UCSBOrganization is a dining commons at UCSB
  */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-@Entity(name = "ucsborganizations")
+@Entity(name = "ucsbOrganization")
 public class UCSBOrganization {
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private String orgCode;
+  @Id private String orgCode;
 
   private String orgTranslationShort;
   private String orgTranslation;
