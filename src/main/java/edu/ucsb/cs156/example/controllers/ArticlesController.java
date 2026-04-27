9db35dfb723e4a1f01889bc2679654df1881a99f
Nick Kamenica <nicholaskamenica@ucsb.edu>
2026-04-27 14:35:23 -0700

    dokku config changes for local environment

--- src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
+++ src/main/java/edu/ucsb/cs156/example/controllers/ArticlesController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-/** This is a REST controller for Articles */
 @Tag(name = "Articles")
 @RequestMapping("/api/articles")
 @RestController
